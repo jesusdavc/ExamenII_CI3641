d8106d195449fd04043be2932ae0382c63ef0751
Jesús David Cuéllar <jedave.co@gmail.com>
2024-11-03 23:33:06 -0400

    Pregunta 1b i

--- pregunta1_b1.cpp
+++ pregunta1_b1.cpp
@@ -1,7 +1,14 @@
+//Universidad Simón Bolívar
+//CI-3641 Lenguajes de Programación I - Examen II
+//Jesús David Cuéllar Carné 15-10345
+
+//Pregunta 1.b i
+
 #include <iostream>
 using namespace std;
 
-// Función f(n) que define las reglas de la función
+//Función f(n) que se define a partir de las reglas dadas
+//en el enunciado. 
 int f(int n) {
     if (n % 2 == 0) {
         return n / 2;
@@ -10,7 +17,8 @@ int f(int n) {
     }
 }
 
-// Función dist(n) que aplica f de manera repetida y cuenta las aplicaciones hasta llegar a 1
+// Función dist(n) que aplica f de manera repetida 
+// y cuenta las aplicaciones hasta llegar a 1
 int dist(int n) {
     int count = 0;
     while (n != 1) {
@@ -19,9 +27,9 @@ int dist(int n) {
     }
     return count;
 }
-
+//Se ejecuta el programa principal. 
 int main() {
-    int n = 42;
+    int n = 42; //Se usa el ejemplo dado en el enunciado
     //cout << "Ingrese un numero entero: ";
     //cin >> n;
     cout << "count(" << n << ") = " << dist(n) << endl;
