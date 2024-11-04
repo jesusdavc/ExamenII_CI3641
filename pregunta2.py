36188055b893f14b9e7adcb1e53d5484ac6775ae
Jesús David Cuéllar <jedave.co@gmail.com>
2024-11-04 18:25:21 -0400

    Inicio Pregunta 4

--- pregunta2.py
+++ pregunta2.py
@@ -1,3 +1,9 @@
+#Universidad Simón Bolívar
+#CI-3641 Lenguajes de Programación I - Examen II
+#Jesús David Cuéllar Carné 15-10345
+
+#Pregunta 2
+
 import operator
 
 # Diccionario de operadores con precedencia y función
---
+++ pregunta4.py
@@ -0,0 +1,5 @@
+#Universidad Simón Bolívar
+#CI-3641 Lenguajes de Programación I - Examen II
+#Jesús David Cuéllar Carné 15-10345
+
+#Pregunta 4
