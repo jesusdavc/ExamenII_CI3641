de8958f730d046ef70efe1e83eb1d422a61b5163
Jesús David Cuéllar <jedave.co@gmail.com>
2024-11-04 23:27:28 -0400

    Pregunta 4. Código Terminado. Falta ampliar más el analisis

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ Faltan pruebas unitarias.
 ## Pregunta 4:
 
 Las tres versiones: Recursiva, Recursiva de Cola e Iterativa se encuentran implementadas. Estan en el archivo **pregunta4.py**
+### Analisis
+Al probar las implementaciones varias veces se puede notar que la versión iterativa consume mucho más tiempo de ejecución. Sin emebargo la diferencia entre la recursión y la recursión de cola si bien entre ellas no es mucha si se ve la tendencia a que la recursión de cola es mucho más rápida. 
--- pregunta4.py
+++ pregunta4.py
@@ -5,6 +5,7 @@
 #Pregunta 4
 
 from collections import deque
+import time
 
 #Subrutina recursiva
 def recursive_alpha_beta(n, alpha = 5, beta = 7):
@@ -42,11 +43,56 @@ def iterative_alpha_beta(n, alpha=5, beta=7):
     return result
 
 def main():
-    print(recursive_alpha_beta(40))
-    print(recursive_tail_alpha_beta(40))
-    print(iterative_alpha_beta(40))
+    # Dentro del programa principal declaramos tres arreglos donde
+    # guardaremos los resultados de varias llamadas con varios valores
+    # a estas llamadas se les calcula el tiempo de visualización usando 
+    # la libreria time. Ahora bien luego con esos resultados se saca el tiempo de ejecución promedio para
+    # cada implementación y de allí se pueden sacar algunas concluciones.
+    recursive_alpha_beta_results =[]
+    recursive_tail_alpha_beta_results =[]
+    iterative_alpha_beta_results = []
+    temporal = 0
+    media_r = 0
+    media_rt = 0
+    media_i = 0
+    for i in range(36,40):
 
+        t1 = time.time() 
+        recursive_alpha_beta(i)
+        f1 = time.time()
+        e1 = f1 - t1
+        recursive_alpha_beta_results.append(e1)
 
+        t2 = time.time() 
+        recursive_tail_alpha_beta(i)
+        f2 = time.time()
+        e2 = f2 - t2
+        recursive_tail_alpha_beta_results.append(e2)
+        
+        t3 = time.time() 
+        iterative_alpha_beta(i)
+        f3 = time.time()
+        e3 = f3 - t3
+        iterative_alpha_beta_results.append(e3)
+
+    for i in recursive_alpha_beta_results:
+        temporal += i
+    media_r = temporal/len(recursive_alpha_beta_results)
+    temporal = 0
+
+    for i in recursive_tail_alpha_beta_results:
+        temporal += i
+    media_rt = temporal/len(recursive_tail_alpha_beta_results)
+    temporal = 0
+
+    for i in iterative_alpha_beta_results:
+        temporal += i
+    media_i = temporal/len(iterative_alpha_beta_results)
+    temporal = 0
+
+    print(f'La media de ejecuión de la recursión fue:         {media_r}')
+    print(f'La media de ejecuión de la recursión de cola fue: {media_rt}')
+    print(f'La media de ejecuión de la versión iterativa    : {media_i}')
 if __name__ == "__main__":
     main()
 
